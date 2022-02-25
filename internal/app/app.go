3d1f2adbcf618e19baf63e868e64827ac9a9eaa0
Arteandr <artedamatik@gmail.com>
2022-02-25 22:29:19 +0300

    Add http server

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,16 +1,18 @@
 package app
 
 import (
-	"fmt"
 	"log"
 	"shop_backend/internal/config"
 )
 
 func Run(configPath string) {
-	cfg, err := config.Init(configPath)
+	// Config
+	_, err := config.Init(configPath)
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
-	fmt.Println(cfg.HTTP.Port)
+
+	// HTTP server
+
 }
