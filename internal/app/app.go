8b8073f57e908912527dd9c87f3ac447259334ed
Arteandr <artedamatik@gmail.com>
2022-02-26 20:26:36 +0300

    Add connection string

--- internal/app/app.go
+++ internal/app/app.go
@@ -19,13 +19,12 @@ func Run(configPath string) {
 	// DB
 	connectionString := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
 		cfg.PGSQL.Host, cfg.PGSQL.Port, cfg.PGSQL.User, cfg.PGSQL.Password, cfg.PGSQL.DatabaseName, cfg.PGSQL.SSLMode)
-
-	db, err := sqlx.Connect("postgres", connectionString)
+	_, err = sqlx.Connect("postgres", connectionString)
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
-	db.Exec("SELECT *")
+
 	// HTTP server
 
 }
