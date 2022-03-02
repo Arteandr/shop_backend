6ffc46cdca5f0e79918677fe64fedd83d3b62936
Arteandr <artedamatik@gmail.com>
2022-03-02 16:42:54 +0300

    Add sign-in

--- internal/delivery/http/v1/users.go
+++ internal/delivery/http/v1/users.go
@@ -12,11 +12,12 @@ func (h *Handler) InitUsersRoutes(api *gin.RouterGroup) {
 		auth := users.Group("/auth")
 		{
 			auth.POST("/sign-up", h.signUp)
+			auth.POST("/sign-in", h.signIn)
 		}
 	}
 }
 
-type signUpInput struct {
+type authInput struct {
 	Email    string `json:"email" binding:"required,email,max=255"`
 	Password string `json:"password" binding:"required,min=6,max=64"`
 }
@@ -26,11 +27,11 @@ type signUpInput struct {
 // @Description Users sign-up
 // @Accept json
 // @Produce json
-// @Param input body signUpInput true "sign-up input"
+// @Param input body authInput true "sign-up input"
 // @Success 200 {string} string "ok"
 // @Router /users/auth/sign-up [post]
 func (h *Handler) signUp(ctx *gin.Context) {
-	var body signUpInput
+	var body authInput
 	if err := ctx.BindJSON(&body); err != nil {
 		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
 			"error": err.Error(),
@@ -56,5 +57,28 @@ func (h *Handler) signUp(ctx *gin.Context) {
 
 	ctx.JSON(http.StatusOK, gin.H{
 		"id": id,
+	})
+}
+
+func (h *Handler) signIn(ctx *gin.Context) {
+	var body authInput
+	if err := ctx.BindJSON(&body); err != nil {
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	tokens, err := h.services.Users.SignIn(body.Email, body.Password)
+	if err != nil {
+		ctx.AbortWithStatusJSON(http.StatusNotFound, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	ctx.SetCookie("refreshToken", tokens.RefreshToken, 2592000, "/", h.cfg.HTTP.Host, false, true)
+	ctx.JSON(http.StatusOK, gin.H{
+		"accessToken": tokens.AccessToken,
 	})
 }
