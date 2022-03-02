7cc910cf532e9b90c1fda5f65fc6a9144642fd67
Arteandr <artedamatik@gmail.com>
2022-03-02 23:23:35 +0300

    Add item model

--- internal/models/item.go
+++ internal/models/item.go
@@ -1,1 +1,12 @@
 package models
+
+import "time"
+
+type Item struct {
+	Id          int       `json:"id" db:"id"`
+	Name        string    `json:"name" db:"name"`
+	Description string    `json:"description" db:"description"`
+	CategoryId  int       `json:"categoryId" db:"category_id"`
+	Tags        []string  `json:"tags" db:"tags"`
+	CreatedAt   time.Time `json:"createdAt" db:"created_at"`
+}
