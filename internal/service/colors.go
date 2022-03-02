246c7876ed7a35e23b7a98da1f9a5a0da6f86e7a
Arteandr <artedamatik@gmail.com>
2022-03-03 00:30:25 +0300

    Add create color method

--- internal/service/colors.go
+++ internal/service/colors.go
@@ -1,6 +1,9 @@
 package service
 
-import "shop_backend/internal/repository"
+import (
+	"shop_backend/internal/models"
+	"shop_backend/internal/repository"
+)
 
 type ColorsService struct {
 	repo repository.Colors
@@ -8,4 +11,19 @@ type ColorsService struct {
 
 func NewColorsService(repo repository.Colors) *ColorsService {
 	return &ColorsService{repo: repo}
+}
+
+func (s *ColorsService) Create(name, hex string, price float64) (int, error) {
+	color := models.Color{
+		Name:  name,
+		Hex:   hex,
+		Price: price,
+	}
+
+	id, err := s.repo.Create(color)
+	if err != nil {
+		return 0, err
+	}
+
+	return id, nil
 }
