e9b42cdcec368bf4611c1b1e6167ebb8cc0d4d9f
Arteandr <artedamatik@gmail.com>
2022-08-06 00:45:02 +0300

    Update user model

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,8 +1,10 @@
 package models
 
 type User struct {
-	Id       int    `json:"id,omitempty" db:"id"`
-	Login    string `json:"login" db:"login"`
-	Email    string `json:"email" db:"email"`
-	Password string `json:"password,omitempty" db:"password"`
+	Id        int    `json:"id,omitempty" db:"id"`
+	Login     string `json:"login" db:"login"`
+	Email     string `json:"email" db:"email"`
+	Password  string `json:"password,omitempty" db:"password"`
+	FirstName string `json:"firstName,omitempty" db:"first_name"`
+	LastName  string `json:"lastName,omitempty" db:"last_name"`
 }
