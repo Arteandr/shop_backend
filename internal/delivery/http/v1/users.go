e3f0a7c165cc9d835e4235b3db0f10969d838161
Arteandr <artedamatik@gmail.com>
2022-08-05 19:12:53 +0300

    Add request body validation

--- internal/delivery/http/v1/users.go
+++ internal/delivery/http/v1/users.go
@@ -1,14 +1,19 @@
 package v1
 
 import (
+	"errors"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"net/mail"
+	"regexp"
 )
 
 func (h *Handler) InitUsersRoutes(api *gin.RouterGroup) {
 	users := api.Group("/users")
 	{
 		users.POST("/sign-up", h.userSignUp)
+		users.POST("/sign-in")
 	}
 }
 
@@ -16,6 +21,41 @@ type userSignUpInput struct {
 	Login    string `json:"login" binding:"required"`
 	Email    string `json:"email" binding:"required"`
 	Password string `json:"password" binding:"required"`
+}
+
+func (user *userSignUpInput) isValidEmail() error {
+	if _, err := mail.ParseAddress(user.Email); err != nil {
+		return errors.New("wrong email")
+	}
+
+	const emailLength = 30
+	if len(user.Email) > emailLength {
+		return errors.New(fmt.Sprintf("email length must not exceed %d characters", emailLength))
+	}
+
+	return nil
+}
+
+func (user *userSignUpInput) isValidLogin() error {
+	if len(user.Login) < 2 || len(user.Login) > 15 {
+		return errors.New("wrong login length")
+	}
+
+	// Include all latin alphabet and numbers 0-9
+	const loginPattern = `^[A-Za-z0-9]+$`
+	if matched, _ := regexp.MatchString(loginPattern, user.Login); !matched {
+		return errors.New("wrong login")
+	}
+
+	return nil
+}
+
+func (user *userSignUpInput) isValidPassword() error {
+	if len(user.Password) < 6 || len(user.Password) > 16 {
+		return errors.New("wrong password length")
+	}
+
+	return nil
 }
 
 // @Summary User SignUp
@@ -32,6 +72,21 @@ type userSignUpInput struct {
 func (h *Handler) userSignUp(ctx *gin.Context) {
 	var body userSignUpInput
 	if err := ctx.BindJSON(&body); err != nil {
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, ErrorResponse{Error: err.Error()})
+		return
+	}
+
+	if err := body.isValidEmail(); err != nil {
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, ErrorResponse{Error: err.Error()})
+		return
+	}
+
+	if err := body.isValidLogin(); err != nil {
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, ErrorResponse{Error: err.Error()})
+		return
+	}
+
+	if err := body.isValidPassword(); err != nil {
 		ctx.AbortWithStatusJSON(http.StatusBadRequest, ErrorResponse{Error: err.Error()})
 		return
 	}
