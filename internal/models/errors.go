435c8967daf41e99efae33e45cdf30219fc5e503
Arteandr <artedamatik@gmail.com>
2022-08-05 19:30:29 +0300

    Add custom user not found error

--- internal/models/errors.go
+++ internal/models/errors.go
@@ -5,4 +5,5 @@ import "errors"
 var (
 	ErrEmptyAuthHeader   = errors.New("empty auth header")
 	ErrInvalidAuthHeader = errors.New("invalid auth header")
+	ErrUserNotFound      = errors.New("user not found")
 )
