7c4a8d21e28dca3e67ce0b7e81a6a69be615e0c9
Arteandr <artedamatik@gmail.com>
2022-08-06 23:17:32 +0300

    Add addresses to user struct

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,11 +1,13 @@
 package models
 
 type User struct {
-	Id        int     `json:"id,omitempty" db:"id"`
-	Login     string  `json:"login" db:"login"`
-	Email     string  `json:"email" db:"email"`
-	Password  string  `json:"password,omitempty" db:"password"`
-	FirstName *string `json:"firstName,omitempty" db:"first_name"`
-	LastName  *string `json:"lastName,omitempty" db:"last_name"`
-	Phone     *string `json:"phone,omitempty" db:"phone"`
+	Id              int      `json:"id,omitempty" db:"id"`
+	Login           string   `json:"login" db:"login"`
+	Email           string   `json:"email" db:"email"`
+	Password        string   `json:"password,omitempty" db:"password"`
+	FirstName       *string  `json:"firstName,omitempty" db:"first_name"`
+	LastName        *string  `json:"lastName,omitempty" db:"last_name"`
+	Phone           *string  `json:"phone,omitempty" db:"phone"`
+	InvoiceAddress  *Address `json:"invoiceAddress,omitempty"`
+	ShippingAddress *Address `json:"shippingAddress,omitempty"`
 }
