137005ef3b23da8f941bde83a0c4bb2f7d954984
Arteandr <artedamatik@gmail.com>
2022-08-07 21:58:11 +0300

    Update CORS

--- internal/delivery/http/middleware.go
+++ internal/delivery/http/middleware.go
@@ -9,7 +9,7 @@ func corsMiddleware(c *gin.Context) {
 	c.Header("Access-Control-Allow-Credentials", "true")
 	c.Header("Access-Control-Allow-Origin", "http://localhost:3000")
 	c.Header("Access-Control-Allow-Methods", "*")
-	c.Header("Access-Control-Allow-Headers", "*")
+	c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
 	c.Header("Content-Type", "application/json")
 
 	if c.Request.Method != "OPTIONS" {
