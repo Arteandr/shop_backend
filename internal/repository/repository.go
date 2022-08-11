5b6bd8473e70acb5241ec3fde5f91a88d706fd47
Arteandr <artedamatik@gmail.com>
2022-08-11 21:13:24 +0300

    Change transactor func

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -124,18 +124,6 @@ func NewRepositories(db *sqlx.DB) *Repositories {
 	}
 }
 
-type txKey struct{}
-
-func injectTx(ctx context.Context, tx *sqlx.Tx) context.Context {
-	return context.WithValue(ctx, txKey{}, tx)
-}
-func extractTx(ctx context.Context) *sqlx.Tx {
-	if tx, ok := ctx.Value(txKey{}).(*sqlx.Tx); ok {
-		return tx
-	}
-	return nil
-}
-
 type SqlxDB interface {
 	ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error)
 	GetContext(ctx context.Context, dest interface{}, query string, args ...interface{}) error
