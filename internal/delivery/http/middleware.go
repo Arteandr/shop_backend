eea1116f985ef0be60ae238f74c6392807cfbbe7
Arteandr <artedamatik@gmail.com>
2022-09-19 17:59:47 +0300

    Change allow methods header

--- internal/delivery/http/middleware.go
+++ internal/delivery/http/middleware.go
@@ -12,7 +12,7 @@ func (h *Handler) corsMiddleware(c *gin.Context) {
 	}
 
 	c.Header("Access-Control-Allow-Credentials", "true")
-	c.Header("Access-Control-Allow-Methods", "*")
+	c.Header("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE")
 	c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
 	c.Header("Content-Type", "application/json")
 
